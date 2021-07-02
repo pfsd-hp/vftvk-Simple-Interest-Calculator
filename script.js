0989c06fa9bf6e03ff1588d67bc33d8bbd5d1900
pfsd-hp <86254086+pfsd-hp@users.noreply.github.com>
2021-07-03 02:18:55 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit "<mark>+principal+</mark>",\<br\>at an interest rate of "<mark>+rate+</mark>"%\<br\>You will receive an amount of "<mark>+amount+</mark>",\<br\>in the year "<mark>+year+</mark>"\<br\>"
 }
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
+}
+function principal_validation() {
+  let x = document.getElementById("principal").value;
+  if (isNaN(x) || x <= 0 ) {
+    alert ("Enter a positive number");
+  } else {
+  }
 }
